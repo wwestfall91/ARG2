51d2cf0df7a991906d9b6909f7155daeb2492639
Wade Westfall <wwestfall91@gmail.com>
2023-11-19 18:59:52 -0500

    Implement Website Skeleton

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <button className="dont-click">Dont Click Me</button>
+      <div className="container">
+        <div className="welcome-text">I'm glad you're finally here</div>
+        <button className="enter-button">ENTER</button>
+      </div>
+    </>
+  );
+}
+
+function OnClickDontClick() {}
+
+export default App;
